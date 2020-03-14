3c1fb1ae4001d0f8f4243f7cf60f94bdf04fe681
yyyyu <g592842897@gmail.com>
2020-03-15 02:22:32 +0800

    publish

--- src/bilibili-judgement.rollup.js
+++ src/bilibili-judgement.rollup.js
@@ -10,8 +10,10 @@ export default {
 // @name         Bilibili 风纪委员投票
 // @namespace    Bilibili
 // @version      0.3
-// @author       yuuuu
+// @author       yyyyu
 // @description  选择多数意见进行投票
+// @license      MIT
+// @homepage     https://github.com/yyyyu/tampermonkey-scripts
 // @icon         https://www.bilibili.com/favicon.ico
 // @match        https://www.bilibili.com
 // @match        https://www.bilibili.com/judgement*
--- src/jd-assistant.rollup.js
+++ src/jd-assistant.rollup.js
@@ -10,9 +10,11 @@ export default {
 // @name         京东助理
 // @namespace    JD
 // @version      0.2
+// @author       yyyyu
 // @description  一键保价、复制购物车选中链接
+// @license      MIT
+// @homepage     https://github.com/yyyyu/tampermonkey-scripts
 // @icon         https://www.jd.com/favicon.ico
-// @author       yuuuu
 // @match        https://pcsitepp-fm.jd.com/
 // @match        https://cart.jd.com/*
 // ==/UserScript==
