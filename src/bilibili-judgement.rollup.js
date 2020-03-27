4b8b3e2cc94423b334beb7c18f7c38fb0e48470d
yyyyu <g592842897@gmail.com>
2020-03-28 00:10:03 +0800

    bi: 0.5 添加投票补偿、描述性文字

--- src/bilibili-judgement.rollup.js
+++ src/bilibili-judgement.rollup.js
@@ -9,8 +9,11 @@ export default {
 // ==UserScript==
 // @name         Bilibili 风纪委员投票
 // @namespace    Bilibili
-// @version      0.4
-// @change-log   添加配置提交内容及投票间隔UI
+// @version      0.5
+// @change-log   0.5 添加投票补偿、描述性文字
+//               案件刚开始审理时投票不多，少量票数不能反映总体趋势，添加补偿票数减少
+//               和主流意见差异。处理案件过程中在界面Slogan UI上添加描述状态的文字
+// @change-log   0.4 添加配置提交内容及投票间隔UI
 // @author       yyyyu
 // @description  选择多数意见进行投票
 // @license      MIT
--- src/bilibili-judgement.ts
+++ src/bilibili-judgement.ts
@@ -1,8 +1,22 @@
-window.onload = async function main() {
+window.onload = scriptMain
+
+// 默认赞成票数补偿
+const DEFAULT_APPROVE_ALTER = 2
+// 默认最小投票间隔
+const DEFAULT_MIN_INTERVAL = 2000
+// 每日最大案件数
+const DAILY_MAX_CASE = 20
+
+async function scriptMain() {
+  const sloganEl = getSloganEl()
   if (location.pathname !== '/') {
     renderSetting()
   }
   if (GM_getValue('finish_day') === getDay()) {
+    if (sloganEl) {
+      setSlogan(sloganEl, `(${DAILY_MAX_CASE}/${DAILY_MAX_CASE})今日已完成任务`)
+    }
+    GM_setValue('today_completed_count', '0')
     return
   }
   if (!(await isFJWer())[0]) {
@@ -12,9 +26,19 @@ window.onload = async function main() {
     GM_openInTab('//www.bilibili.com/judgement', { insert: true })
     return
   }
+  let todayCount = parseInt(GM_getValue('today_completed_count', '0'), 10)
   while (true) {
+    if (sloganEl) {
+      setSlogan(sloganEl, `(${todayCount}/${DAILY_MAX_CASE})获取案件...`)
+    }
     const [caseID, code] = await getCaseID()
     if (code === 25008) {
+      if (sloganEl) {
+        setSlogan(
+          sloganEl,
+          `(${todayCount}/${DAILY_MAX_CASE})当前无案件，5s后自动重试`,
+        )
+      }
       await delay(5000)
       location.reload()
       return
@@ -24,8 +48,28 @@ window.onload = async function main() {
       window.close()
       return
     }
+    if (sloganEl) {
+      setSlogan(sloganEl, `(${todayCount}/${DAILY_MAX_CASE})获取案件票数...`)
+    }
+    // 被审理案件违规的情况更普遍，投票早期偶有发生投反对导致最终结果出现差异
+    let approveALter = parseInt(
+      GM_getValue('approve_alter', `${DEFAULT_APPROVE_ALTER}`),
+      10,
+    )
+    approveALter = Number.isInteger(approveALter)
+      ? approveALter
+      : DEFAULT_APPROVE_ALTER
     const approve =
-      (await getVoteCount(caseID, 1))[0] >= (await getVoteCount(caseID, 2))[0]
+      (await getVoteCount(caseID, 1))[0] + approveALter >=
+      (await getVoteCount(caseID, 2))[0]
+    if (sloganEl) {
+      setSlogan(
+        sloganEl,
+        `(${todayCount}/${DAILY_MAX_CASE})案件投${
+          approve ? '赞成' : '反对'
+        }票...`,
+      )
+    }
     await vote(
       caseID,
       approve,
@@ -33,10 +77,22 @@ window.onload = async function main() {
         ? GM_getValue('approve_text', '')
         : GM_getValue('disapprove_text', ''),
     )
-    let intervel = parseInt(GM_getValue('vote_min_interval', '2000'), 10)
+    GM_setValue('today_completed_count', `${++todayCount}`)
+    let intervel = parseInt(
+      GM_getValue('vote_min_interval', `${DEFAULT_MIN_INTERVAL}`),
+      10,
+    )
     intervel =
-      (Number.isInteger(intervel) && intervel > 0 ? intervel : 0) +
+      (Number.isInteger(intervel) && intervel >= 0
+        ? intervel
+        : DEFAULT_MIN_INTERVAL) +
       Math.round(Math.random() * 2) * 1000
+    if (sloganEl) {
+      setSlogan(
+        sloganEl,
+        `(${todayCount}/${DAILY_MAX_CASE})${intervel}ms后处理下一案件`,
+      )
+    }
     await delay(intervel)
   }
 }
@@ -158,6 +214,20 @@ function getDay() {
 }
 
 /**
+ * 获取口号 element
+ */
+function getSloganEl() {
+  return document.querySelector<HTMLDivElement>('.fjw-user .u-img')
+}
+
+/**
+ * 设置口号
+ */
+function setSlogan(el: HTMLDivElement, text: string) {
+  el.innerText = text
+}
+
+/**
  * 渲染设置UI
  */
 function renderSetting() {
@@ -198,10 +268,15 @@ function createForm(hide: () => void) {
     '反对描述',
     GM_getValue('disapprove_text', ''),
   )
+  const approveALterRow = createInputRow(
+    '赞成修正(判定投票结果时添加到赞成方):',
+    '赞成修正',
+    GM_getValue('approve_alter', `${DEFAULT_APPROVE_ALTER}`),
+  )
   const intervelRow = createInputRow(
     '最小投票间隔(ms)(实际会额外增加0~2s):',
     '最小投票间隔(ms)',
-    GM_getValue('vote_min_interval', '2000'),
+    GM_getValue('vote_min_interval', `${DEFAULT_MIN_INTERVAL}`),
   )
   const buttonGroup = document.createElement('div')
   buttonGroup.className = 'votescript-buttongroup'
@@ -217,9 +292,21 @@ function createForm(hide: () => void) {
     GM_setValue('disapprove_text', disapproveText)
     disapproveInput.value = disapproveText
 
+    const approveALterInput = approveALterRow.lastElementChild as HTMLInputElement
+    let approveALter = parseInt(approveALterInput.value, 10)
+    approveALter =
+      Number.isInteger(approveALter) && approveALter >= 0
+        ? approveALter
+        : DEFAULT_APPROVE_ALTER
+    GM_setValue('approve_alter', `${approveALter}`)
+    approveALterInput.value = `${approveALter}`
+
     const intervelInput = intervelRow.lastElementChild as HTMLInputElement
     let intervel = parseInt(intervelInput.value, 10)
-    intervel = Number.isInteger(intervel) && intervel >= 0 ? intervel : 2000
+    intervel =
+      Number.isInteger(intervel) && intervel >= 0
+        ? intervel
+        : DEFAULT_MIN_INTERVAL
     GM_setValue('vote_min_interval', `${intervel}`)
     intervelInput.value = `${intervel}`
 
@@ -228,7 +315,13 @@ function createForm(hide: () => void) {
   const cancelBtn = createButton('取消')
   cancelBtn.addEventListener('click', () => hide())
   buttonGroup.append(saveBtn, cancelBtn)
-  form.append(approveRow, disapproveRow, intervelRow, buttonGroup)
+  form.append(
+    approveRow,
+    disapproveRow,
+    approveALterRow,
+    intervelRow,
+    buttonGroup,
+  )
   return form
 }
 
@@ -272,78 +365,78 @@ function createStyle() {
   style.appendChild(
     document.createTextNode(`
 .votescript-cfgbtn {
--webkit-appearance: none;
-position: fixed;
-bottom: 20px;
-right: 0px;
-width: 100px;
-height: 32px;
-line-height: 32px;
-background-color: #00a1d6;
-border: none;
-border-radius: 4px;
-color: #fff;
-font-size: 16px;
-font-weight: 700;
-cursor: pointer;
+  -webkit-appearance: none;
+  position: fixed;
+  bottom: 20px;
+  right: 0px;
+  width: 100px;
+  height: 32px;
+  line-height: 32px;
+  background-color: #00a1d6;
+  border: none;
+  border-radius: 4px;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
+  cursor: pointer;
 }
 
 .votescript-modal {
-position: fixed;
-z-index: 1000;
-top: 0;
-left: 0;
-width: 100vw;
-height: 100vh;
-background-color: rgba(0, 0, 0, 0.3);
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.3);
 }
 
 .votescript-modal.hide {
-display: none;
+  display: none;
 }
 
 .votescript-form {
-box-sizing: border-box;
-width: 500px;
-margin: 100px auto 0;
-padding: 15px;
-background-color: #fff;
-border-radius: 5px;
+  box-sizing: border-box;
+  width: 500px;
+  margin: 100px auto 0;
+  padding: 15px;
+  background-color: #fff;
+  border-radius: 5px;
 }
 
 .votescript-row {
-margin: 10px 0;
+  margin: 10px 0;
 }
 
 .votescript-buttongroup {
-margin-top: 20px;
+  margin-top: 20px;
 }
 
 .votescript-btn {
--webkit-appearance: none;
-margin-right: 10px;
-padding: 4px 12px;
-border: none;
-border-radius: 3px;
-background-color: #00a1d6;
-color: #fff;
-cursor: pointer;
+  -webkit-appearance: none;
+  margin-right: 10px;
+  padding: 4px 12px;
+  border: none;
+  border-radius: 3px;
+  background-color: #00a1d6;
+  color: #fff;
+  cursor: pointer;
 }
 
 .votescript-label {
-display: inline-block;
-margin-bottom: 5px;
+  display: inline-block;
+  margin-bottom: 5px;
 }
 
 .votescript-input {
--webkit-appearance: none;
-display: block;
-box-sizing: border-box;
-width: 100%;
-height: 30px;
-border: 1px solid #c1c1c1;
-border-radius: 3px;
-padding: 2px 5px;
+  -webkit-appearance: none;
+  display: block;
+  box-sizing: border-box;
+  width: 100%;
+  height: 30px;
+  border: 1px solid #c1c1c1;
+  border-radius: 3px;
+  padding: 2px 5px;
 }
 `),
   )
